ff6b423a178989448fe6f0eb5859df202e39b536
ahmedseragcodes <ahmedserag1@gmail.com>
2021-10-20 17:08:48 -0400

    created dbconfig

--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,20 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './dev.sqlite3'
+    },
+    migrations: {
+      directory: 
+    },
+    seeds: {
+
+    },
+    pool: {
+      afterCreate: ((conn, done)=>{
+        conn.run("PRAGMA foreign_keys = ON", done);
+      })
     }
   },
 
