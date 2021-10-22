1f5eac348f3c87478ac2287d966dffb2d4590269
ahmedseragcodes <ahmedserag1@gmail.com>
2021-10-22 16:12:12 -0400

    wrote modeling for getting call by id and updating call by id and with updated call object

--- callsAPI/callsAPIModel.js
+++ callsAPI/callsAPIModel.js
@@ -4,15 +4,37 @@ const db = require("../dbConfig");
 
 //Modeling
 
+//[GET] All Calls
+
 const getAllCalls = () => {
 
     return db("calls");
+};
+
+//[GET] Call By Call ID
+
+const getCallById = async (idOfCallToGet) => {
+
+    const call = await db("calls").where("id", idOfCallToGet);
+
+    return call;
+    
+};
+
+//[PUT] Update Call By Call ID
+
+const updateCall = async (updatedCall) => {
+
+    const updatedCallId = updatedCall.id;
+
+    const result = db("calls").where("id", updatedCallId).insert(updatedCall);
+
+    return result;
+
 }
-
-
 
 // Exports 
 
 module.exports = {
-    getAllCalls,
+    getAllCalls, getCallById, updateCall
 }
